c8b6d464aae7d4297231e83fe3800cc62e1f7f48
evilcode <teng_ontheway@163.com>
2017-12-11 11:06:47 +0800

    test

--- js/config/Constants.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by evilcode on 2017/12/6.
- */
-export const TEST_CONSTANT = "TEST_CONSTANT"
--- js/core/config/CoreConstant.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by evilcode on 2017/12/4.
- */
-
-export const TOKEN_STORAGE_KEY = "TokenStorageKey"
-
-
-
